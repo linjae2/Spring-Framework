984b8842c2cb13a66f5579c6699d49bb52b2b7d7
Lee In-Jae <injae@snudh.org>
2023-06-17 06:55:18 +0900

    스프링 브트에서는 jsp 사용하는 것을 건장하지 않고 있어, 내장된 톱켓은 JSP 엔진을 포함하지 않는다.
    의존성 추가 => tomcat-embed-jasper

--- 01. Tomcat 연동/02. spring boot/src/main/java/com/example/demo/DemoApplication.java
+++ 01. Tomcat 연동/02. spring boot/src/main/java/com/example/demo/DemoApplication.java
@@ -2,16 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
+	@GetMapping("/")
+	String getIndex(ModelMap model) {
+		model.addAttribute("message", "Hello Spring MVC Framework!");
+		return "hello";
 	}
 
 	public static void main(String[] args) {
